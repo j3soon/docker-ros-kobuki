e6fbc159c4190d7fc0aea83cabcd8b1f4a263d37
Johnson Sun <j3.soon777@gmail.com>
2023-05-18 22:40:30 +0800

    Add kobuki udev rules setup script

---
+++ setup_udev_rules.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Ref: https://kobuki.readthedocs.io/en/devel/troubleshooting.html#problems-solutions
+sudo cp "$DIR/thirdparty/60-kobuki.rules" /etc/udev/rules.d
+sudo service udev reload
+sudo service udev restart
+read -p "Please (re-)connect Kobuki now, and Press enter to continue."
+ls /dev/kobuki
